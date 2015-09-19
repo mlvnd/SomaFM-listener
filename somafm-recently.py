f594e906afc6c85dfd3ffb885b937c0469677cdc
Melvin Dijkstra <melvin.dijkstra@gmail.com>
2015-09-19 11:23:37 +0200

    Added groove salad.

--- somafm-recently.py
+++ somafm-recently.py
@@ -3,49 +3,41 @@ import requests
 import sys
 from time import sleep
 
-stations = {
+STATIONS = {
     'defcon': 'http://somafm.com/recent/defcon.html'
+    'groovesalad': 'http://somafm.com/recent/groovesalad.html'
 }
 
 def now_playing(station):
-    lines = requests.get(stations[station]).text.splitlines()
+    results = []
+    lines = requests.get(STATIONS[station]).text.splitlines()
     while len(lines) > 0:
         line = lines.pop(0)
-        if line == u'<!-- line 1 -->':
+        if line == '<!-- line 1 -->':
             break
     lines.pop(0)
     line = lines.pop(0)
-    
-    results = []
-    buff = ""
+
+    buff = ''
     in_tag = False
     for char in line:
         if char == '<':
+            in_tag = True
             if len(buff) > 0:
                 results.append(buff)
-                buff = ""
-    
-            in_tag = True
-    
+                buff = ''
         elif char == '>':
             in_tag = False
-    
         elif not in_tag:
             buff += char
-    
+ 
     while len(results) < 3:
         results.append("")
-    
+
     return results[0:3]
 
-
-
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        station = 'defcon'
-    else:
-        station = sys.argv[1]
-
+    station = sys.argv[1] if len(sys.argv) < 2 else 'defcon'
     old = None
     try:
         while True:
